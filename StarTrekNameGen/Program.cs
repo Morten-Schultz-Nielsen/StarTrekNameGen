5fcdd092d87c4067d795881cae8cadfaad175f51
mort358d <mort358d@CV-BB-5860>
2019-08-12 11:58:44 +0200

    Added more names

--- StarTrekNameGen/Program.cs
+++ StarTrekNameGen/Program.cs
@@ -38,14 +38,109 @@ namespace StarTrekNameGen
                     new string[] { "r", "j", "'p", "k", "l" }
                 }
             };
+            string[][][] TalaxianMaleLetters = new string[][][]
+            {
+                new string[][] 
+                {
+                    new string[] { "B", "Br", "D", "J", "L", "N", "P", "T", "W" },
+                    new string[] { "e", "i", "u", "a" },
+                    new string[] { "x" },
+                    new string[] { "i", "ia", "e", "o" },
+                    new string[] { "th", "n", "m" }
+                },
+                new string[][]
+                {
+                    new string[] { "B", "Br", "D", "J", "L", "N", "P", "T", "W" },
+                    new string[] { "e", "i", "u", "a" },
+                    new string[] { "x", "xa", "x", "xe" },
+                }
+            };
+            string[][][] TalaxianFemaleLetters = new string[][][]
+            {
+                                new string[][]
+                {
+                    new string[] { "A", "E", "O" },
+                    new string[] { "l", "ld" },
+                    new string[] { "i", "a" },
+                    new string[] { "x" },
+                    new string[] { "i", "ia", "e", "o" },
+                    new string[] { "th", "n", "m" }
+                },
+                new string[][]
+                {
+                    new string[] { "A", "E", "O" },
+                    new string[] { "x" },
+                    new string[] { "e", "i", "u", "a" },
+                    new string[] { "l", "ld" },
+                    new string[] { "i", "ia", "e", "o" },
+                    new string[] { "th", "n", "m" }
+                }
+            };
+            string[][][] KlingonsMaleLetters = new string[][][]
+            {
+                new string[][]
+                {
+                    new string[] { "K", "Kr", "B", "Br", "Ch", "L", "D", "Dr", "M", "G", "H", "J", "N", "K", "Kl", "P", "R", "S", "Sh", "St", "T" },
+                    new string[] { "o", "e", "ee", "u", "a", "i", "y", "aa" },
+                    new string[] { "rt", "nt", "r", "t", "lg", "rk", "nn", "ggr", "hl", "v" },
+                    new string[] { "o", "e", "u", "a", "i", "y" },
+                    new string[] { "r", "n", "t", "l", "x" },
+                    new string[] { "o", "e", "u", "a", "i", "y" },
+                    new string[] { "r", "k", "ng", "n", "x", "q", "g" }
+                },
+                new string[][]
+                {
+                    new string[] { "K", "Kr", "B", "Br", "Ch", "L", "D", "Dr", "M", "G", "H", "J", "N", "K", "Kl", "P", "R", "S", "Sh", "St", "T" },
+                    new string[] { "o", "e", "ee", "u", "a", "i", "y", "aa" },
+                    new string[] { "rt", "nt", "r", "t", "lg", "rk", "nn", "ggr", "hl", "v" },
+                    new string[] { "o", "e", "u", "a", "i", "y" },
+                    new string[] { "r", "n", "t", "l", "x", "q", "g" },
+                },
+                new string[][]
+                {
+                    new string[] { "K", "Kr", "B", "Br", "Ch", "L", "D", "Dr", "M", "G", "H", "J", "N", "K", "Kl", "P", "R", "S", "Sh", "St", "T" },
+                    new string[] { "o", "e", "ee", "u", "a", "i", "y", "aa" },
+                    new string[] { "rt", "nt", "r", "t", "lg", "rk", "nn", "rg", "hl", "v" },
+                },
+                new string[][]
+                {
+                    new string[] { "A", "O" },
+                    new string[] { "rt", "nt", "r", "t", "lg", "rk", "nn", "ggr", "hl", "v" },
+                    new string[] { "o", "e", "ee", "u", "a", "i", "y", "aa" },
+                    new string[] { "r", "k", "ng", "n", "x", "q", "g" }
+                }
+            };
+            string[][][] KlingonsFemaleLetters = new string[][][]
+            {
+                new string[][]
+                {
+                    new string[] { "K'", "B'", "Ch'", "D'", "L'", "G'", "M'", "N'", "J'", "T'", "Hij'" },
+                    new string[] { "K", "Kr", "B", "Br", "Ch", "L", "D", "Dr", "M", "G", "H", "J", "N", "K", "Kl", "P", "R", "S", "Sh", "St", "T" },
+                    new string[] { "o", "e", "ee", "u", "a", "i", "y", "aa" },
+                    new string[] { "rt", "nt", "r", "t", "lg", "rk", "nn", "ggr", "hl", "v" },
+                    new string[] { "o", "e", "u", "a", "i", "y" },
+                    new string[] { "r", "n", "t", "l", "x", "q", "g" },
+                },
+                new string[][]
+                {
+                    new string[] { "K'", "B'", "Ch'", "D'", "L'", "G'", "M'", "N'", "J'", "T'" },
+                    new string[] { "K", "Kr", "B", "Br", "Ch", "L", "D", "Dr", "M", "G", "H", "J", "N", "K", "Kl", "P", "R", "S", "Sh", "St", "T" },
+                    new string[] { "o", "e", "ee", "u", "a", "i", "y", "aa" },
+                    new string[] { "rt", "nt", "r", "t", "lg", "rk", "nn", "rg", "hl", "v" },
+                },
+            };
             Random random = new Random();
 
             while(true)
             {
                 //get input and parse it
-                Console.WriteLine("Skriv først hvilken type navn og derefter antallet af navne");
+                Console.WriteLine("Skriv først hvilken type navn og derefter antalet af navne");
                 Console.WriteLine("Vulcane Male: VM");
                 Console.WriteLine("Vulcane Female: VF");
+                Console.WriteLine("Talaxian Male: TM");
+                Console.WriteLine("Talaxian Female: TF");
+                Console.WriteLine("Klingon Male: KM");
+                Console.WriteLine("Klingon Female: KF");
                 Console.WriteLine("Eksempel: VM100 ");
                 string input = Console.ReadLine();
                 if(input.Length < 3)
@@ -54,8 +149,7 @@ namespace StarTrekNameGen
                     continue;
                 }
                 string nameType = input.Substring(0,2);
-                int number = 0;
-                if(!int.TryParse(input.Substring(2), out number) || number == 0) //parse the number of names to get
+                if(!int.TryParse(input.Substring(2), out int number) || number == 0) //parse the number of names to get
                 {
                     Console.WriteLine("Ugyldigt input!");
                     continue;
@@ -69,6 +163,18 @@ namespace StarTrekNameGen
                         break;
                     case "VF":
                         genNameFrom = VulcanFemaleLetters;
+                        break;
+                    case "TM":
+                        genNameFrom = TalaxianMaleLetters;
+                        break;
+                    case "TF":
+                        genNameFrom = TalaxianFemaleLetters;
+                        break;
+                    case "KM":
+                        genNameFrom = KlingonsMaleLetters;
+                        break;
+                    case "KF":
+                        genNameFrom = KlingonsFemaleLetters;
                         break;
                     default:
                         Console.WriteLine("Ugyldigt input!");
